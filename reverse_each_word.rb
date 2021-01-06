bc4d358a73f24c8ac115456dc950bbc94ac9a627
Aaron Muesing <muesing.music@gmail.com>
2021-01-06 00:46:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(sentence)
   array = []
-  sentence.split.collect { |sentence| sentence.reverse << array.join }
+  sentence.split.collect { |sentence| sentence.reverse.join( )}
 end
